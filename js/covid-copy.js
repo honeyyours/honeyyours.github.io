3bc063b596f0bf1bc19655826583bca99cf7815e
honeyyours <75129776+honeyyours@users.noreply.github.com>
2020-12-29 21:51:36 +0900

    change

--- js/covid-copy.js
+++ js/covid-copy.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-var url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson'; /*URL*/
+var url = 'https://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson'; /*URL*/
 var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+'ZTA0ThFuL3LhjBoSmkJi7q6KH45jxcI570l0pkn5UX20KxU0%2F3VRuNUf%2FjiK27E7BDnOuXW7JTdBd0eJiNDzdg%3D%3D'; /*Service Key*/
 queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
 queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
