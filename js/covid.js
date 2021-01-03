277d1b72b6d7da9c1de6e8f8f6bffac3ee503621
honeyyours <75129776+honeyyours@users.noreply.github.com>
2021-01-03 13:37:58 +0900

    change API

--- js/covid.js
+++ js/covid.js
@@ -6,11 +6,32 @@ const TODAY = "20201229";
 
 function getweather() {
   fetch(
-    `http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=${API_KEY_COVID}&pageNo=1&numOfRows=10&startCreateDt=${TODAY}&endCreateDt=${TODAY}`,
-    { method: "get", mode: "same-origin" }
-  ).then(function (response) {
-    console.log(response);
-  });
+    `http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey=ZTA0ThFuL3LhjBoSmkJi7q6KH45jxcI570l0pkn5UX20KxU0%2F3VRuNUf%2FjiK27E7BDnOuXW7JTdBd0eJiNDzdg%3D%3D&pageNo=1&numOfRows=10&startCreateDt=${TODAY}&endCreateDt=${TODAY}`,
+    {
+      method: "GET",
+      headers: {
+        // the content type header value is usually auto-set
+        // depending on the request body
+        "Content-Type": "text/plain;charset=UTF-8",
+      },
+      mode: "cors",
+      credentials: "same-origin", // omit, include
+      cache: "default", // no-store, reload, no-cache, force-cache, or only-if-cached
+      redirect: "follow", // manual, error
+      integrity: "", // a hash, like "sha256-abcdef1234567890"
+      keepalive: false, // true
+      signal: undefined, // AbortController to abort request
+      window: window, // null
+    }
+  )
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (response) {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(response, "application/xml");
+      console.log(xml);
+    });
 }
 
 function checktime() {
